beaf3b11e82a52ab2b00490fedee4c2411eb1be9
HDubuis <helene.dubuis@cpnv.ch>
2022-06-20 13:40:59 +0200

    Add an example for config.js file

---
+++ MOB1-Nextep/example.config.js
@@ -0,0 +1,4 @@
+export const config = { 
+    apiurl: 'http://10.229.33.168:8000/api/nxp/', //'https://mockapi.mycpnv.ch/api/nxp/'
+    imageUrl: 'http://10.229.33.168:8000/storage/pics/'  //'https://mockapi.mycpnv.ch/storage/pics/'
+}
