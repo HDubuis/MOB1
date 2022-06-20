3aeea9c7aa33ad3e0fb3010ee669f961ade06cb9
HDubuis <helene.dubuis@cpnv.ch>
2022-06-20 16:32:27 +0200

    Update example.config.js

--- MOB1-Nextep/example.config.js
+++ MOB1-Nextep/example.config.js
@@ -1,4 +1,4 @@
 export const config = { 
-    apiurl: 'http://10.229.33.168:8000/api/nxp/', //'https://mockapi.mycpnv.ch/api/nxp/'
-    imageUrl: 'http://10.229.33.168:8000/storage/pics/'  //'https://mockapi.mycpnv.ch/storage/pics/'
+    apiurl: 'http://example.com/api/nxp/', 
+    imageUrl: 'http://example.com/storage/pics/'
 }
